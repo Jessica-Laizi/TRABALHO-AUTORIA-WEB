9da495e24b62c693168c3b0cc39212b36f4c7122
Laizi <jessicalaizisilva@gmail.com>
2024-12-04 21:58:01 -0300

    trabalho com todas as páginas solicitadas

---
+++ server.php
@@ -0,0 +1,33 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informações cadastradas</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+        <h1>DOAÇÃO SOLIDÁRIA</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">1</a></li>
+                <li><a href="server.php">2</a></li>
+                <li><a href="desenvolvedores.html">3</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class = "preencher">
+    <?php
+    print_r($_POST)
+    ?>
+    </main>
+    <footer>
+        <p>IFRN Campus Santa Cruz</p>
+        <p>Trabalho de Autoria Web</p>
+        <p>Prof. Marcelo Figueiredo Barbosa Júnior</p>
+        <p>Componentes do grupo: Beatriz Lauanny Soares dos Santos e Jéssica Laizi da Silva</p>
+        <p>Referências: [https://www.devmedia.com.br/html-input-construindo-formularios-com-html/43493]</p>
+    </footer>
+</body>
+</html>
